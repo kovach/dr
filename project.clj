6078b5a32187c78e7cca92e910ba830223770733
Scott Kovach <dskovach@gmail.com>
2014-06-20 18:52:52 -0700

    lein stuff

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject dr "1.0.0-SNAPSHOT"
+  :description "a mathematical parser"
+  :dependencies [[org.clojure/clojure "1.3.0"]])
---
+++ test/dr/test/core.clj
@@ -0,0 +1,6 @@
+(ns dr.test.core
+  (:use [dr.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
